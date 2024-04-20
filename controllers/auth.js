02caaa05ec1e4c65a621d017a605a9e15c8ae0d5
sabbir <sabbir@gmail.com>
2024-04-20 22:57:34 +0600

    register done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,41 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 export const register = async (req, res) => {
-  //   console.log(req.body);
-  //   try {
-  //     const {
-  //       firstName,
-  //       lastName,
-  //       email,
-  //       password,
-  //       picturePath,
-  //       friends,
-  //       location,
-  //       occupation,
-  //       birthDate,
-  //     } = req.body;
-  //     const salt = await bcrypt.genSalt();
-  //     const passwordHash = await bcrypt.hash(password, salt);
-  //     const newUser = new User({
-  //       firstName,
-  //       lastName,
-  //       email,
-  //       password: passwordHash,
-  //       picturePath,
-  //       friends,
-  //       location,
-  //       occupation,
-  //       viewedProfile: Math.floor(Math.random() * 10000),
-  //       impressions: Math.floor(Math.random() * 10000),
-  //     });
-  //     const savedUser = await newUser.save();
-  //     res.status(201).json(savedUser);
-  //   } catch (err) {
-  //     res.status(500).json({ error: err.message });
-  //   }
+  try {
+    const birthDate = "01 january 2021";
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      birthDate,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res
+      .status(201)
+      .json({ message: "user create successfully", data: savedUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something wrong. try again", error });
+  }
 };
