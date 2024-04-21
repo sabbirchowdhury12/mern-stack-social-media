7d67a9959e660f60089b3064804938703c2e47c9
sabbir <sabbir@gmail.com>
2024-04-21 22:12:37 +0600

    post controller done

---
+++ controllers/post.js
@@ -0,0 +1,75 @@
+import Post from "../models/post.js";
+import User from "../models/user.js";
+
+/* CREATE */
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    const post = await Post.find();
+    res.status(201).json({ message: "create post sucessfully", data: post });
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+/* READ */
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ message: "Fetched post sucessfully", data: posts });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const posts = await Post.find({ userId });
+    res.status(200).json({ message: "Fetched post sucessfully", data: posts });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res
+      .status(200)
+      .json({ message: "Updated post sucessfully", data: updatedPost });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import {
+  createPost,
+  getAllPosts,
+  getUserPosts,
+  likePost,
+} from "../controllers/post.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /* READ */
-router.get("/", verifyToken, getFeedPosts);
+router.post("/create-post", verifyToken, createPost);
+router.get("/", verifyToken, getAllPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
